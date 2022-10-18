1d7b21681c982e33500fed0b44a5844a4d9f3f9a
Lim JaeKwon <l77715227@gmail.com>
2022-10-18 11:45:15 +0900

    help. regex. apropos. findFn

---
+++ 4. 도움말 🔑 도움말 문서 목록, help(), help.search(), R SEEK.R
@@ -0,0 +1,24 @@
+help.start()
+help(median)
+?median
+args(median)
+example(median)
+
+help.search("xyplot")
+??xyplot
+
+hero.vector <- c("superman", "batman", "spiderman")
+apropos("vector")
+?regex #정규포현식이란...
+
+apropos("q$")
+apropos("[7-9]")
+apropos("xy+")
+apropos("xy")
+
+
+RSiteSearch("topicmodel")
+install.packages("sos")
+library(sos)
+
+findFn("social network analysis")
